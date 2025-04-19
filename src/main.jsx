0b3dd83228269d7cfb138abe4295a46fe470c873
DylanBlanco <dylanblanco1198@gmail.com>
2025-04-19 14:08:38 +0200

    Counter Component - creazione componente

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,16 @@ import { createRoot } from 'react-dom/client'
 // import './index.css'
 import App from './App.jsx'
 
+import Counter from './components/counter.jsx'
+
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <h1>
             Questo è l'inizio di React
         </h1>
         <App />
+        
+        {/* Counter con nuovo Componente */}
+        <Counter />
     </StrictMode>,
 )
