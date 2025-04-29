4fa3c11de3e2c66d555e28d8f2a59690cf0f374c
DylanBlanco <dylanblanco1198@gmail.com>
2025-04-29 17:31:29 +0200

    Input Array - stampato array

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -5,8 +5,20 @@ function Input() {
     
     const [userData, setUserData] = useState('')
     const [userLeng, setUserLeng] = useState('')
+    const [schoolObjects, setSchoolObjects] = useState([
+        { name: "Matita", category: "Scrittura", price: 0.50 },
+        { name: "Penna", category: "Scrittura", price: 0.80 },
+        { name: "Gomma", category: "Correzione", price: 0.30 },
+        { name: "Righello", category: "Misurazione", price: 1.00 },
+        { name: "Quaderno", category: "Cartoleria", price: 1.50 },
+        { name: "Temperamatite", category: "Accessori", price: 0.70 },
+        { name: "Evidenziatore", category: "Scrittura", price: 1.20 },
+        { name: "Colla stick", category: "Creatività", price: 1.00 },
+        { name: "Forbici", category: "Creatività", price: 2.50 },
+        { name: "Zaino", category: "Contenitore", price: 20.00 }
+    ])
     
-    //  Input
+    //  Input - function di interpolazione
     function changeUserData (e) { // event - per ottenere valore digitato da utente
         // const element = e.target // target - per ottenere il vaolre dell'input
         // const v = element.value
@@ -16,7 +28,7 @@ function Input() {
         setUserData(e.target.value) // funzione ridotta
     }
 
-    // Select
+    // Select - function di interpolazione
     function selChangeUserData (e) { // event - per ottenere valore digitato da utente
         // const element = e.target // target - per ottenere il vaolre dell'input
         // const v = element.value
@@ -65,6 +77,37 @@ function Input() {
                                     </select>
                                 </div>
                                 <div className="mini-text">{userLeng}</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="cardJsx">
+                        <h3>ARRAY</h3>
+                        <div className="input-card">
+                            <h4>Oggetti Scolastici</h4>
+                            {
+                                schoolObjects.map((obj, i) => (
+                                    <p key={obj + '_' + i}>{i + 1}. {obj.name}</p>
+                                ))
+                            }
+                        </div>
+                        <div className="input-card">
+                            <div className="row">
+                                <div>
+                                    <h4>Cerca</h4>
+                                    <input type="text" />
+                                </div>
+
+                                <div>
+                                    <h4>Filtra</h4>
+                                    <select>
+                                        <option value="">Category</option>
+                                        <option value="">Category</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <hr />
+                            <div className="row">
+                                <p>-Risultato ricerca-</p>
                             </div>
                         </div>
                     </div>
