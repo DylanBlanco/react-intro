a8a2cfba2b48b5638c3a7ed53ddbdb876b22139a
DylanBlanco <dylanblanco1198@gmail.com>
2025-05-19 15:19:52 +0200

    UseEffect

--- src/App3.jsx
+++ src/App3.jsx
@@ -1,10 +1,22 @@
+import { useEffect } from 'react'
 import './css/app3.css'
 function App() {
+    // Logica ---------------------------------------
+    useEffect(() => {
+        console.log('re-rendering effettuato!')
+    })
 
 
+    // Template -------------------------------------
     return(
         <div className="app3">
             <h1>App3</h1>
+            <div className='container-app3'>
+                <h2>Use-Effect</h2>
+                <div className="cardJsx">
+                    qlq                  
+                </div>
+            </div>
         </div>
     )
 }
