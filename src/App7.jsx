7cfd9c61776926dd3ee7ebe60b28ac7c65f67378
DylanBlanco <dylanblanco1198@gmail.com>
2025-05-28 17:49:07 +0200

    App7 - UseContext - descrizione

--- src/App7.jsx
+++ src/App7.jsx
@@ -1,3 +1,4 @@
+import { useContext } from 'react'
 import './css/app7.css'
 
 
@@ -6,7 +7,12 @@ export default function App() {
         <div className="app7">
             <h1>App7</h1>
             <div className="container-app7">
-
+                <h2>UseContext</h2>
+                <div>
+                    <p>
+                        è un hook di React che ti permette di accedere a un contesto (context) direttamente all'interno di un componente funzionale, <br /> senza dover passare manualmente le props da componente a componente (il cosiddetto "prop drilling").
+                    </p>
+                </div>
             </div>
         </div>
     )
