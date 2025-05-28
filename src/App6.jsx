a152ef7bad145f7bb43582e776f4153d5f56cbd5
DylanBlanco <dylanblanco1198@gmail.com>
2025-05-28 16:30:39 +0200

    App6 - custom hook - function useCounter(startValue)

--- src/App6.jsx
+++ src/App6.jsx
@@ -1,7 +1,9 @@
+import { useState } from 'react'
 import './css/app6.css'
 
 function App() {
     // Logica  ------------------------------------------------
+    const {counter, incrementCounter, decrementCounter} = useCounter(10)
 
 
     // Template  ----------------------------------------------
@@ -10,9 +12,32 @@ function App() {
             <h1>App6</h1>
             <div className="container-app6">
                 <h2>Custom Hook</h2>
+                <div>
+                    <p>Descrizione Hook</p>
+                </div>
+
+                <div className="row">
+                    <div className="cardJsx">
+
+                    </div>
+                </div>
             </div>
         </div>
     )
+}
+
+function useCounter(startValue) {
+    const [counter, setCounter] = useState(startValue)
+
+    function incrementCounter() {
+        setCounter(c => c + 1)
+    }
+
+    function decrementCounter() {
+        setCounter(c => c - 1)
+    }
+
+    return {counter, incrementCounter, decrementCounter}
 }
 
 export default App
